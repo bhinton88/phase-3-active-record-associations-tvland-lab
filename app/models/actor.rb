d9bbd0c00df5a2b0273157457e5e772ced2a1e6a
Bobby Hinton <bob.g.hint@gmail.com>
2022-11-26 12:21:33 -0600

    commit tree a3423886f09a4b07ecf883d357269aa0b1722557

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    first_name = self.first_name
+    last_name = self.last_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    character_name = self.characters.first.name
+    show_name = self.shows.first.name
+    "#{character_name} - #{show_name}"
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    first_name = self.actors.first.first_name
+    last_name = self.actors.first.last_name
+    "#{first_name} #{last_name}"
+  end
+
 end
